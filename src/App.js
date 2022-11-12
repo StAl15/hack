2732d050a252df2df6030f24ec9993bf83cda547
StAl15 <stihinaleksander@yandex.ru>
2022-11-12 11:58:29 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './login/Login'
+import Registration from './registration/Registration'
+import ListConsultant from './list_consultant/ListConsultant'
+import Profile from './profile/Profile'
+import Chat from './chat/Chat'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/list_consultant" element={<ListConsultant />} />
+      </Routes>
+    </Router>
+
   );
 }
 
