a8b063a33da3f3e4e3c978d52f1788d327d555c7
Miroslav <sutula05@bk.ru>
2022-11-12 15:55:05 +0500

    123

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,22 +1,56 @@
 import React from "react";
 import './Login.css';
 import { TextField, Button } from "@mui/material"
+import {useNavigate} from 'react-router-dom';
+
+import { useSelector, useDispatch } from 'react-redux'
+import { setCredentials } from "../redux/slice/AuthSlice";
+import { useLoginMutation } from "../redux/slice/AuthApiSlice";
+
+
 // import ico_sign from "../media/ico_sign.svg"
 
 const Login = () => {
+    const userRef = React.useRef()
+    const errRef = React.useRef()
+    const [user, setUser] = React.useState('')
+    const [pwd, setPwd] = React.useState('')
+    const [errMsg, setErrMsg] = React.useState('')
+    const navigate = useNavigate()
+
+    const [login, {isLoading}] = useLoginMutation()
+    const dispatch = useDispatch()
+
+    React.useEffect(() => {
+        
+    }, [user, pwd]);
+
+    const handlesumbit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const userData = await login({user, pwd}).unwrap()
+            dispatch(setCredentials({...userData, user}))
+            setUser('')
+            setPwd('')
+            navigate('chat')
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div className="login_content">
             <div className="login_left_side">
 
                 <h2 className="title_signin">Вход</h2>
 
-                <TextField fullWidth id="login_input" label="Логин" variant="outlined" style={{}} />
+                <TextField ref={userRef} value={user} onChange={(e)=> setUser(e.target.value)} fullWidth id="login_input" label="Логин" variant="outlined" style={{}} />
 
-                <TextField fullWidth id="password_input" label="Пароль" type="password" variant="outlined" style={{ marginTop: 15, }} />
+                <TextField  value={pwd} onChange={(e)=> setPwd(e.target.value)} fullWidth id="password_input" label="Пароль" type="password" variant="outlined" style={{ marginTop: 15, }} />
 
                 <a>Забыли пароль?</a>
 
-                <Button style={{ marginTop: 15, backgroundColor: "#39B6EC", }} variant="contained" disableElevation>
+                <Button onClick={(e)=> handlesumbit(e)} style={{ marginTop: 15, backgroundColor: "#39B6EC", }} variant="contained" disableElevation>
                     Войти
                 </Button>
                 <p>Нет аккаунта? Создать бесплатно</p>
