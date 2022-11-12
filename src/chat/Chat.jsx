08bb368fceb0c4afa9a88231eeb70a05dd2daefc
Miroslav <sutula05@bk.ru>
2022-11-12 19:17:24 +0500

    123

---
+++ src/chat/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Chat = () => {
+    return (
+        <div>
+            <Link to={"/profile"}>Профиль </Link>
+        </div>
+    );
+};
+
+export default Chat;
