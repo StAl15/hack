4cc7c3ca6f2237c971f89d913f9c8923bfa862cd
StAl15 <stihinaleksander@yandex.ru>
2022-11-12 13:07:04 +0500

    upd

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import './Login.css';
+import { TextField, Button } from "@mui/material"
+// import ico_sign from "../media/ico_sign.svg"
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className="login_content">
+            <div className="login_left_side">
+
+                <h2 className="title_signin">Вход</h2>
+
+                <TextField fullWidth id="login_input" label="Логин" variant="outlined" style={{}} />
+
+                <TextField fullWidth id="password_input" label="Пароль" type="password" variant="outlined" style={{ marginTop: 15, }} />
+
+                <a>Забыли пароль?</a>
+
+                <Button style={{ marginTop: 15, backgroundColor: "#39B6EC", }} variant="contained" disableElevation>
+                    Войти
+                </Button>
+                <p>Нет аккаунта? Создать бесплатно</p>
+
+            </div>
+
+            <div className="login_right_side">
+
+            </div>
+
         </div>
 
     )
